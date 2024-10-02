929b11c11fd4760553fc697112104a5c6562aaaf
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-10-02 18:54:05 +0200

    updaye 2

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
+  };
+
+  return (
+    <div>
+      <p>
+        x: {values.x}, y: {values.y}
+      </p>
+
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
+    </div>
+  );
+};
+
+export default App;
